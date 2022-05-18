c1958e767d8eb6b3c7ed25bca400f0013bfc296c
omiras <oscar.miras@gmail.com>
2022-05-18 12:43:04 +0200

    updated

---
+++ script9.js
@@ -0,0 +1,13 @@
+// Implementa en este fichero la solución
+// Escribe antes todos los pasos que hay que realizar con lenguaje natural
+
+// Estas palabras prohibidas vienen de la base de datos. Úsa la variable palabrasProhibidas, porque sus valores podrían cambiar en función de las necesidades del censor 
+
+let palabrasProhibidas = ["Bugatti", "yummmy", "booty"];
+
+console.log("Es hora de censurar!")
+
+// Resultado esperado: tras cargar la aplicación Web, las palabras contenidas en el array 'palabrasProhibidas' han sido substituidas por **** en el documento Web ya renderizado.
+
+// Pista: Fíjate que todas las palabras candidatas a ser prohibidas estan dentro de un <span>
+// Pista: REcuerda que existe un método de array muy interesante para saber si un elemento esta dentro de un array: https://www.w3schools.com/jsref/jsref_includes_array.asp
